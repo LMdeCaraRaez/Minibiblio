033f51ca5cec5c1f80902ec738fdd453664ad811
LMdeCaraRaez <luismigueldecararaez@gmail.com>
2025-01-22 09:42:03 +0100

    Se crea la entidad socio

---
+++ src/Entity/Socio.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SocioRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SocioRepository::class)]
+class Socio
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, unique: true)]
+    private ?string $dni = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $apellidos = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $nombre = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDni(): ?string
+    {
+        return $this->dni;
+    }
+
+    public function setDni(string $dni): static
+    {
+        $this->dni = $dni;
+
+        return $this;
+    }
+
+    public function getApellidos(): ?string
+    {
+        return $this->apellidos;
+    }
+
+    public function setApellidos(?string $apellidos): static
+    {
+        $this->apellidos = $apellidos;
+
+        return $this;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(?string $nombre): static
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+}
