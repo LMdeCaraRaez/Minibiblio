24f83742f712fb1ef8af2dfc3b6c86c95b152437
LMdeCaraRaez <luismigueldecararaez@gmail.com>
2025-03-05 14:39:14 +0100

    Se hace parte de las cosas con voters

--- src/Security/LibroVoter.php
+++ src/Security/LibroVoter.php
@@ -2,19 +2,61 @@
 
 namespace App\Security;
 
+use App\Entity\Libro;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Security;
 
-class LibroVoter extends  Voter
+class LibroVoter extends Voter
 {
+    private Security $security;
 
-    protected function supports(string $attribute, $subject)
+    public function __construct(Security $security)
     {
-        // TODO: Implement supports() method.
+        $this->security = $security;
+    }
+
+    const LIST = "LIBRO_LIST";
+    const VIEW = "LIBRO_VIEW";
+    const DELETE = "LIBRO_DELETE";
+    const EDIT = "LIBRO_EDIT";
+
+
+
+    protected function supports(string $attribute, $subject): bool
+    {
+
+        if (!in_array($attribute, [self::LIST, self::VIEW, self::DELETE, self::EDIT], true)) {
+            return false;
+        }
+
+        if (!$subject instanceof Libro) {
+            return false;
+        }
+
+
+        return true;
     }
 
     protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token)
     {
-        // TODO: Implement voteOnAttribute() method.
+        $user = $token->getUser();
+
+        if (!$subject instanceof Libro) {
+            return false;
+        }
+
+        switch ($attribute) {
+            case self::LIST:
+                return $this->canList();
+            case self::DELETE:
+                return $this->security->isGranted("ROLE_ADMIN") && $subject->getSocio() === null;
+            case self::VIEW:
+                return $this->canView();
+            case self::EDIT:
+                return $this->security->isGranted("ROLE_BIBLIOTECARIO") || $this->security->isGranted("ROLE_DOCENTE") && $user === $subject->getSocio();
+        }
+
+        return false;
     }
 }
